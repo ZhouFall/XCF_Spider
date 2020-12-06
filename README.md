0b3403694dd1672b2cae20fdfa5d3504449acb3d
Zhou Fall <18860901456@163.com>
2020-12-06 21:19:13 +0800

    update comments and readme files

--- README.md
+++ README.md
@@ -1,36 +1,34 @@
+#### 工程说明
+##### 1. 找到下厨房网站中，用户懒饭所有的菜谱
+##### 2. 找到菜谱中的关键信息，菜谱名，实物图，用料和做法
+##### 3. 按照给出的docx模板，将找到的信息填入到world里，并汇总成一张总表
+***
+#### Todo:
+1. 由于下载700多个网页和对应的图片，所以总时长在20分钟左右，word的频繁读写也占用了很多时间。需要考虑多线程来缩短等待时间
+2. 执行过程中只有打印消息，那个菜谱下载了保存了word,可以加入一个进度条，方便知道程序的执行进度
+3. 增加requirement.txt,列出所有需要安装的包
+4. 食材那里，有些食物没有写分量，导致匹配关系错乱，需要采用食物数量这种方式来组合，而不是分别放进两个list再组成字典
+***
+#### 遇到的问题
 
-
-
-1.查看最后的输出文件，json只有743个数据，少了7个，排查思路如下
-2.网站链接是25页，一页是30个，一共750个，从保存的html文件里搜索div，数量相符
-3.排查res部分，加上打印，每个foods打印count
-4.排除掉这部分之后，怀疑是zip函数的问题，内存不够用，丢数据了
-5.换成itertools额izip，报错找不到izip，查看源码，方法换成了zip_longest
-6.打印之后，发现还是少了几位
-7.怀疑是list变成dict的时候，有重复数据，怎么找出这个重复数据？
-8.>>> from collections import Counter
-    >>> Counter([1,2,2,2,2,3,3,3,4,4,4,4])
+##### 25页，每页30个食谱，应该输出50个数据，最后只有743个
+1. 查看最后的输出文件，json只有743个数据，少了7个，排查思路如下
+2. 网站链接是25页，一页是30个，一共750个，从保存的html文件里搜索div，数量相符
+3. 排查res部分，加上打印，每个foods打印count
+4. 排除掉这部分之后，怀疑是zip函数的问题，内存不够用，丢数据了
+5. 换成itertools额izip，报错找不到izip，查看源码，方法换成了zip_longest，打印之后，发现还是少了几位
+6. 怀疑是list变成dict的时候，有重复数据，怎么找出这个重复数据？
+7.  使用counter方法
+    ```
+    from collections import Counter
+    Counter([1,2,2,2,2,3,3,3,4,4,4,4])
     Counter({1: 5, 2: 3, 3: 2})
-9.真相大白
-    Counter({'鸡蛋汉堡': 3, '红茶芝士蛋糕': 2, '香菇鸡腿焖饭': 2, '圆白菜蟹棒烩豆腐': 2, '烂蒜肥肠': 2, '快手番茄锅': 2, '鲜虾干蒸烧卖': 1,
-
-print(len(title))
-print(len(link))
-# xyz = itertools.zip(title, link)
-# print(xyz)
-d = dict(itertools.zip_longest(title, link))
-# print(d)
-print(len(d))
-
-word常用的方法有add_heading, add_paragraph,add_table
+    ```
+    `Counter({'鸡蛋汉堡': 3, '红茶芝士蛋糕': 2, '香菇鸡腿焖饭': 2, '圆白菜蟹棒烩豆腐': 2, '烂蒜肥肠': 2, '快手番茄锅': 2, '鲜虾干蒸烧卖': 1,}`
 
 ##### 在替换word中图片时遇到问题，总是报错图片找不到，学会解析docx的xml文件很有用
-1.使用的方法为：document.replace_pic('图片 1',r'../output/word/{}.jpg'.format(title))
-2.删除原来模板里的图片，重新上传pic.jpg图片，图片名字使用pic.jpg不行
-3.参考网上的教程，将docx文件后缀名改为.zip，然后解压，查看xml文件，发现word,media下面有image1.jpg
-  尝试使用image1.jpg作为文件名，还是失败
-4.查看document.xml文件，找到  <w:drawing>标签，发现图片名字是'图片 1'
-  使用图片 1.jpg/jpeg，都不行
-5.最后直接使用图片 1，成功。
-
-##### 合并所有文件到一个文件的时候，先打开一个模板文件，这样格式不会乱
+1. 使用的方法为：`document.replace_pic('图片 1',r'../output/word/{}.jpg'.format(title))`
+2. 删除原来模板里的图片，重新上传pic.jpg图片，图片名字使用pic.jpg不行
+3. 参考网上的教程，将docx文件后缀名改为.zip，然后解压，查看xml文件，发现word,media下面有image1.jpg,尝试使用image1.jpg作为文件名，还是失败
+4. 查看document.xml文件，找到  <w:drawing>标签，发现图片名字是'图片 1',使用图片 1.jpg/jpeg，都不行
+5. 最后直接使用图片 1，成功。
--- script/food.py
+++ script/food.py
@@ -4,6 +4,8 @@ from time import sleep
 from datetime import datetime
 import json
 import os
+# from collections import Counter    #查找重复数据
+
 from docx import Document
 #导入尺寸类,颜色，字体大小
 from docx.shared import Inches,Pt,RGBColor
@@ -12,11 +14,12 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml.ns import qn
 from docxtpl import DocxTemplate
 from docxcompose.composer import Composer
-# from collections import Counter    #查找重复数据
 
+#下厨房用户懒饭创作的菜谱主页地址
 url = 'https://www.xiachufang.com/cook/126271064/created/?page='
+#本地存放整个工程包的路径
 root_path = r'C:\Users\Administrator\Desktop\Python学习\Spider\food'
-
+#request请求的用户头
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3823.400 QQBrowser/10.7.4307.400',
     'Accept': 'text/html, */*; q=0.01',
@@ -30,14 +33,16 @@ simple_header = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
 
+#申明一个类
 class xiachufang():
     def __init__(self,link,root_path):
+        #初始化赋值
         self.link = link
         self.headers = headers
         self.path = root_path    #root path
         self.html_path = r'../output/html'
-        # print(self.html_path)
-    
+
+    #获取主页上的内容，注意翻页是滑动翻页，不是点击下一页，请求地址后面要加上?page=
     def get_page_content(self):
         print('开始爬下厨房菜单\n')
         title = []
@@ -49,16 +54,19 @@ class xiachufang():
             print('网站链接为：{0}\n爬到的返回状态为{1}\n'.format(self.link, res.status_code))
             print("开始解析数据\n")
             content = BeautifulSoup(res.text, parser='html.parser', features="lxml")
+            #菜谱名字和链接数据
             foods = content.find_all(class_="cover")  # 使用cover更精准
             for food in foods:
                 title.append(food.find('a')['title'])
                 link.append(homepage+food.find('a')['href'])
             #爬完一个网站后,等0.2s再爬下一个网站
             sleep(0.2)
+            #输出到food.html文档
             with open(r'../data/food.html', 'ab+') as f:
                 f.write(res.content)
             print('write success\n')
         # print(Counter(title))    #可用于检验重复的菜名
+        #转换成字典后，重复的菜名将被丢掉，有7个重复的，一共是25页，每页30个菜，减去7个重复的，743道菜谱
         d = dict(zip(title, link))
         j = json.dumps(d,ensure_ascii=False,indent=4)
         with open(r'../data/link.json','w',encoding='utf-8') as f:
@@ -68,13 +76,14 @@ class xiachufang():
         print("菜名和对应的网页链接解析完毕\n")
         sleep(1)
 
+    #根据json文件里面的链接，一个一个把网页内容下载下来
     def download_one_page(self,title,link):
         self.headers['Referer'] = link
         res = requests.get(link,headers=self.headers)
         with open(self.path+'\output\html\{}.html'.format(title),'wb') as f:
             f.write(res.content)
             print('{}下载成功'.format(title))
-
+    #把json中所有的链接都下载下来
     def download_all_page(self):
         with open('../data/link.json', 'rb') as f:
             filejson = json.load(f)
@@ -85,6 +94,7 @@ class xiachufang():
             sleep(0.5)  # 延时0.2s,可以成功，不会封
         print('所有页面下载成功')
 
+    #查找当前路径下所有的html文件
     def walkFile(self):
         for root, dirs, files in os.walk(self.html_path):
             for f in files:
@@ -92,6 +102,7 @@ class xiachufang():
                     fullname = os.path.join(root, f)
                     yield fullname
 
+    #对一个食谱下载下来的网站进行分析
     def parser_one_page(self,file):
         with open(file,'rb') as f:
             soup = BeautifulSoup(f,parser='html.parser',features="lxml")
@@ -155,6 +166,7 @@ class docx_handler():
                     file_name.append(fullname)
         return file_name
 
+    #合并所有的docx文档到一个world里
     def merge_all_docx(self):
         files = self.get_filename()
         print(files)
@@ -169,10 +181,15 @@ class docx_handler():
 if __name__ == '__main__':
     print('下厨房爬虫开始运行,开始时间:')
     print(datetime.now())
+    #对象实例化
     xcf = xiachufang(url,root_path)
+    #找到所有菜谱的名字和网站链接
     xcf.get_page_content()
+    #把所有菜谱的网站链接下载下来
     xcf.download_all_page()
+    #解析好下载下来的html文件，并按模板docx输出
     xcf.parser_all_page()
+    #将输出的docx文件合并成一个
     doc = docx_handler(r'../output/word','docx',r'../output/下厨房懒饭菜谱合集.docx')
     doc.merge_all_docx()
     print('下厨房爬虫运行结束,结束时间:')
